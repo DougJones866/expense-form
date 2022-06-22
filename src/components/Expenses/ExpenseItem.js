758e00f04fe64aced2dc4144345b77b07574e2ae
kibroman <you@example.comkibromanx@gmail.com>
2022-06-21 22:35:06 -0700

    updated

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,12 +6,12 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
-    const [title, setTitle] = useState(props.title);
+    const [title, ] = useState(props.title);
 
 
-    const clickHandler = () => {
-        setTitle('Updated');
-    }
+    // const clickHandler = () => {
+    //     setTitle('Updated');
+    // }
 
 
     return (
@@ -22,7 +22,7 @@ function ExpenseItem(props) {
                 <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
+            {/* <button onClick={clickHandler}>Change Title</button> */}
         </Card>
         </li>
     );
